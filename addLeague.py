8fd0ab9bbb51cb230804a6085fd63c4ce846d2b1
akintilosteeven <steeven-charles-edward.akintilo@epitech.eu>
2025-01-05 22:52:04 +0100

    [FIX] Some bugs

---
+++ addLeague.py
@@ -0,0 +1,35 @@
+from scrapData import * 
+from teamData import *
+# data = teamData()
+l = """
+    https://www.footmercato.net/club/gnk-dinamo-zagreb/classement
+    https://www.footmercato.net/club/fc-salzburg/classement
+    https://www.footmercato.net/club/malmo-ff/classement
+    https://www.footmercato.net/norvege/eliteserien/classement
+    https://www.footmercato.net/club/etoile-rouge-de-belgrade/classement
+    https://www.footmercato.net/club/fc-shakhtar-donetsk/classement
+    https://www.footmercato.net/club/botafogo-fr/classement
+    https://www.footmercato.net/club/ca-boca-juniors/classement
+    https://www.footmercato.net/club/ca-penarol/classement
+    https://www.footmercato.net/club/club-internacional-de-futbol-miami/classement
+    https://www.footmercato.net/chine/chinese-super-league/classement
+    https://www.footmercato.net/club/kf-tirana/classement
+    https://www.footmercato.net/club/maccabi-tel-aviv-fc/classement
+"""
+
+# l = """
+#     https://www.footmercato.net/club/fc-universitatea-cluj-1/classement
+# """
+
+
+# l = l.split("\n")
+
+# for ll in l:
+#     if len(ll) > 3:
+#         S = Scraper()
+#         list_of_team_league(S,ll.strip().replace(" ",""))
+#         time.sleep(5)
+
+# list_of_team_league(S,"https://www.footmercato.net/club/gnk-dinamo-zagreb/classement")
+
+# list_of_team_league(S,"https://www.footmercato.net/club/fc-salzburg/classement")
